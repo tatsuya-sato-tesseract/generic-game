dafbba34b976d5707268060c8e7a35be39329feb
Willem Olding <willemolding@gmail.com>
2019-06-22 14:36:52 +1000

    add tests

--- test/index.js
+++ test/index.js
@@ -23,7 +23,38 @@ const diorama = new Diorama({
 })
 
 
-// <<DEVCAMP>> Your tests here
+diorama.registerScenario("Can create a new game and make moves", async (s, t, { alice, bob }) => {
+  const create_game_result = await alice.callSync('main', 'create_game', {
+    opponent: bob.agentId,
+    timestamp: 0,
+  })
+  console.log(create_game_result)
+  // check the ok response is an address
+  t.equal(create_game_result.Ok.length, 46)
+
+  // bob must make the first move
+  const move_1_result = await bob.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { Place: {x: 0, y: 0 } },
+      timestamp: 0
+    }
+  })
+  console.log(move_1_result)
+  // make sure the move was made without error
+  t.equal(move_1_result.Err, undefined)
+
+  // alice try to make an invalid move
+  const move_2_result = await alice.callSync('main', 'make_move', {
+    new_move: {
+      game: create_game_result.Ok,
+      move_type: { Place: { x: 0, y: 0 } },
+      timestamp: 1,
+    }
+  })
+  // this move is in the same place as before so should fail validation
+  t.equal(move_2_result.Ok, undefined)
+})
 
 
 diorama.run()
